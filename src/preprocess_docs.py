39f656ce46aefb14db67d2f1a1568a0c4c46a6ad
Ruben Peeters <ruben@fampeeters.be>
2024-11-23 17:06:46 +0100

    Remove links from subject. Add pickle file.

--- src/preprocess_docs.py
+++ src/preprocess_docs.py
@@ -1,33 +1,48 @@
 from rdflib import Graph
 from dotenv import load_dotenv
+import pickle
+from pprint import pprint
 
 # Load environment variable for OpenAI API key
 
 if __name__ == "__main__":
     load_dotenv()
-        
+    out = dict()
     g = Graph()
     g.parse("Data/items_filtered.ttl", format="turtle")
     json_ld_data = g.serialize(format="json-ld", indent=4)
-    with open("graph_output.txt", "w") as file:
-        file.write("RDF Graph Triples:\n")
-        file.write("==================\n\n")
-        
+    with open("Data/graph_output.txt", "w") as file:
+       
         # Iterate through each triple in the graph
         subjects = set(g.subjects())
         for subject in subjects:
+            
             org_subject = subject
             if "api/item" in subject:
+                print(subject)
                 subj_formalized = str(subject).split('/')[-1]
                 if '#' in subj_formalized:
                     subject = subj_formalized.split('#')[-1]
                 else:
                     subject = subj_formalized
-            file.write(f"Subject {subject}\n")
-            # Iterate over all triples where this subject is the subject
-            for pred, obj in g.predicate_objects(subject=org_subject):
-                if "http" not in obj:
-                    file.write(f"{pred}: {obj}\n")
+                out[subject] = {}
+                file.write(f"Subject {subject}\n")
+                # Iterate over all triples where this subject is the subject
+                for pred, obj in g.predicate_objects(subject=org_subject):
+                    pred_formalized = str(pred).split('/')[-1]
+                    if '#' in pred_formalized:
+                        pred = pred_formalized.split('#')[-1]
+                    else:
+                        pred = pred_formalized
+                    if "http" not in obj:
+                        out[subject][pred] = str(obj)
+                        file.write(f"{pred}: {obj}\n")
+                     
+    with open("Data/dataset.pkl", "wb") as f:
+        pickle.dump(out, f)
+    with open("Data/dataset.pkl", "rb") as f:
+        new = pickle.load(f)
+        pprint(new)
         # for subj, pred, obj in g:
             # Write the triple in a readable format
             # seen_subjects = set()
