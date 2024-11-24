55c2cc2016346ce34b5de3bfd30ef79b9ab8dfe6
Ruben Peeters <ruben@fampeeters.be>
2024-11-24 13:12:16 +0100

    New dataset first version

--- src/rdf_convert.py
+++ src/rdf_convert.py
@@ -3,5 +3,5 @@ from rdflib import Graph
 # Convert JSON-LD to TTL
 
 g = Graph()
-g.parse("Data/items.json", format="json-ld")
-g.serialize("Data/items.ttl", format="turtle")
+g.parse("Data/new_data.json", format="json-ld")
+g.serialize("Data/new_data.ttl", format="turtle")
